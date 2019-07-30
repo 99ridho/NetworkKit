809927fc1b19a859de31999073f4b35b4930111f
Muhammad Ridho K. Pratama <99ridho@gmail.com>
2019-07-30 13:37:34 +0700

    initialize repository

---
+++ NetworkKit/NetworkKit.h
@@ -0,0 +1,19 @@
+//
+//  NetworkKit.h
+//  NetworkKit
+//
+//  Created by Ridho Pratama on 30/07/19.
+//  Copyright © 2019 Ridho Pratama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkKit.
+FOUNDATION_EXPORT double NetworkKitVersionNumber;
+
+//! Project version string for NetworkKit.
+FOUNDATION_EXPORT const unsigned char NetworkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkKit/PublicHeader.h>
+
+
