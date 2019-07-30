ff77028eac6762c0160e6b05e81f73fd443cc646
Muhammad Ridho K. Pratama <99ridho@gmail.com>
2019-07-31 00:03:38 +0700

    tidying typing

--- NetworkKit/NetworkProvider.swift
+++ NetworkKit/NetworkProvider.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public class NetworkProvider<Request: RequestProtocol> {
     public typealias CompletionHandler = (Data?, URLResponse?, Error?) -> Void
-    public typealias Fetcher = (URLRequest, NetworkProvider.CompletionHandler?) -> CancelableTask
+    public typealias Fetcher = (URLRequest, CompletionHandler?) -> CancelableTask
     
     private var fetcher: Fetcher
     
@@ -24,7 +24,7 @@ public class NetworkProvider<Request: RequestProtocol> {
     }
     
     @discardableResult
-    public func request (_ request: Request, completion: NetworkProvider.CompletionHandler?) -> CancelableTask? {
+    public func request (_ request: Request, completion: CompletionHandler?) -> CancelableTask? {
         let url = request.baseURL.appendingPathComponent(request.path)
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = request.method.rawValue
